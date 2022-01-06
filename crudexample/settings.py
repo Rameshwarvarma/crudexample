db4746cb3c71d2760fa93d065cd89427e4963b99
Rameswara Varma Vatsavayi <rameswara.vatsavayi@wisseninfotech.com>
2022-01-07 02:36:54 +0530

    Allowed Hosts

--- crudexample/settings.py
+++ crudexample/settings.py
@@ -24,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h5ttbfoc_gjc5^m!ubow!(csr3kipj%t-(nz*r^)p*k67qp6u0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+    'expk.herokuapp.com'
+]
 
 
 # Application definition
@@ -43,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
